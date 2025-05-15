9aa8ce2449ab6572300a700fc0ebad9b7ad2163b
rifqiharrys <rifqi.m.h@gmail.com>
2025-05-15 11:20:38 +0700

    remove individual model prediction. leaving only singular function for all available models

--- sdb/modeling.py
+++ sdb/modeling.py
@@ -20,6 +20,7 @@ def prediction(
     ----------
     model : str
         The model to use for prediction. Options are 'knn', 'linear', or 'rf'.
+        See model_alias_dict for more details.
     unraveled_band : pd.DataFrame
         Unraveled raster data.
     features_train : pd.DataFrame
@@ -32,6 +33,10 @@ def prediction(
         The number of jobs to run in parallel. Default is -2.
     **params : dict
         Parameters to pass to the respective model.
+        See sklearn documentation for more details.
+        For KNeighborsRegressor: n_neighbors, weights, algorithm, leaf_size, etc.
+        For LinearRegression: fit_intercept, copy_X, etc.
+        For RandomForestRegressor: n_estimators, criterion, bootstrap, etc.
 
     Returns
     -------
@@ -47,172 +52,28 @@ def prediction(
         )
 
     model_alias_dict = {
-        'knn': {'knn', 'k_nearest_neighbors'},
-        'linear': {'mlr', 'linear', 'linear_regression'},
-        'rf': {'rf', 'random_forest'}
+        'knn': {
+            'knn', 'k_nearest_neighbors', 'K-Nearest Neighbors'
+        },
+        'linear': {
+            'mlr', 'linear', 'linear_regression', 'Multiple Linear Regression'
+        },
+        'rf': {
+            'rf', 'random_forest', 'Random Forest'
+        }
     }
 
-    if model.lower() in model_alias_dict['knn']:
+    if model in model_alias_dict['knn']:
         regressor = KNeighborsRegressor(**params)
-    elif model.lower() in model_alias_dict['linear']:
+    elif model in model_alias_dict['linear']:
         regressor = LinearRegression(**params)
-    elif model.lower() in model_alias_dict['rf']:
+    elif model in model_alias_dict['rf']:
         regressor = RandomForestRegressor(**params)
     else:
         raise ValueError(
             f'Invalid model: {model}.\n'
             f'Allowed: {set.union(*model_alias_dict.values())}'
         )
-
-    with parallel_backend(backend=backend, n_jobs=n_jobs):
-        regressor.fit(features_train, label_train)
-        z_predict = regressor.predict(unraveled_band)
-
-    return z_predict
-
-
-def k_nearest_neighbors(
-        unraveled_band: pd.DataFrame,
-        features_train: pd.DataFrame,
-        label_train: pd.Series,
-        backend: str = 'threading',
-        n_jobs: int = -2,
-        **params
-):
-    """
-    Predicting depth using K-Nearest Neighbors.
-
-    Parameters
-    ----------
-    unraveled_band : pd.DataFrame
-        Unraveled raster data.
-    features_train : pd.DataFrame
-        Features from train data.
-    label_train : pd.Series
-        Label from train data.
-    backend : str, optional
-        Backend to use for parallel processing. Default is 'threading'.
-    n_jobs : int, optional
-        The number of jobs to run in parallel. Default is -2.
-    **params : dict
-        Parameters to pass to KNeighborsRegressor such as
-        n_neighbors, weights, algorithm, leaf_size, etc.
-        See sklearn documentation for more details.
-
-    Returns
-    -------
-    np.ndarray
-        An array of predicted depth from trained model using unraveled raster data.
-    """
-
-    allowed_backend = {'loky', 'threading', 'multiprocessing'}
-    if backend not in allowed_backend:
-        raise ValueError(
-            f'Invalid backend: {backend}.\n'
-            f'Allowed: {allowed_backend}'
-        )
-
-    regressor = KNeighborsRegressor(**params)
-
-    with parallel_backend(backend=backend, n_jobs=n_jobs):
-        regressor.fit(features_train, label_train)
-        z_predict = regressor.predict(unraveled_band)
-
-    return z_predict
-
-
-def linear_regression(
-        unraveled_band: pd.DataFrame,
-        features_train: pd.DataFrame,
-        label_train: pd.Series,
-        backend: str = 'threading',
-        n_jobs: int = -2,
-        **params
-):
-    """
-    Predicting depth using Linear Regression.
-
-    Parameters
-    ----------
-    unraveled_band : pd.DataFrame
-        Unraveled raster data.
-    features_train : pd.DataFrame
-        Features from train data.
-    label_train : pd.Series
-        Label from train data.
-    backend : str, optional
-        Backend to use for parallel processing. Default is 'threading'.
-    n_jobs : int, optional
-        The number of jobs to run in parallel. Default is -2.
-    **params : dict
-        Parameters to pass to LinearRegression such as fit_intercept,
-        copy_X, etc.
-        See sklearn documentation for more details.
-
-    Returns
-    -------
-    np.ndarray
-        An array of predicted depth from trained model using unraveled raster data.
-    """
-
-    allowed_backend = {'loky', 'threading', 'multiprocessing'}
-    if backend not in allowed_backend:
-        raise ValueError(
-            f'Invalid backend: {backend}.\n'
-            f'Allowed: {allowed_backend}'
-        )
-
-    regressor = LinearRegression(**params)
-
-    with parallel_backend(backend=backend, n_jobs=n_jobs):
-        regressor.fit(features_train, label_train)
-        z_predict = regressor.predict(unraveled_band)
-
-    return z_predict
-
-
-def random_forest(
-        unraveled_band: pd.DataFrame,
-        features_train: pd.DataFrame,
-        label_train: pd.Series,
-        backend: str = 'threading',
-        n_jobs: int = -2,
-        **params
-):
-    """
-    Predicting depth using Random Forest.
-
-    Parameters
-    ----------
-    unraveled_band : pd.DataFrame
-        Unraveled raster data.
-    features_train : pd.DataFrame
-        Features from train data.
-    label_train : pd.Series
-        Label from train data.
-    backend : str, optional
-        Backend to use for parallel processing. Default is 'threading'.
-    n_jobs : int, optional
-        The number of jobs to run in parallel. Default is -2.
-    **params : dict
-        Parameters to pass to RandomForestRegressorsuch as n_estimators,
-        criterion, bootstrap, etc.
-        See sklearn documentation for more details.
-
-    Returns
-    -------
-    np.ndarray
-        An array of predicted depth from trained model using unraveled raster data.
-    """
-
-    allowed_backend = {'loky', 'threading', 'multiprocessing'}
-    if backend not in allowed_backend:
-        raise ValueError(
-            f'Invalid backend: {backend}.\n'
-            f'Allowed: {allowed_backend}'
-        )
-
-    regressor = RandomForestRegressor(**params)
 
     with parallel_backend(backend=backend, n_jobs=n_jobs):
         regressor.fit(features_train, label_train)
