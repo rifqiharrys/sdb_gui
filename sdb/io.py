84036092edde59aca3dd2048e1a6b8f5acc2f5b4
rifqiharrys <rifqi.m.h@gmail.com>
2025-09-10 13:40:07 +0800

    add Path input type

--- sdb/io.py
+++ sdb/io.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import Any
 
 import geopandas as gpd
@@ -7,13 +8,13 @@ import xarray as xr
 from pyproj.crs.crs import CRS
 
 
-def read_geotiff(raster_loc: str) -> xr.DataArray:
+def read_geotiff(raster_loc: Path | str) -> xr.DataArray:
     """
     Read Geotiff raster data using Xarray (using rioxarray extension).
 
     Parameters
     ----------
-    raster_loc : str
+    raster_loc : Path | str
         Raster data location.
 
     Returns
@@ -24,13 +25,13 @@ def read_geotiff(raster_loc: str) -> xr.DataArray:
     return rxr.open_rasterio(raster_loc, masked=True) # type: ignore
 
 
-def read_shapefile(vector_loc: str) -> gpd.GeoDataFrame:
+def read_shapefile(vector_loc: Path | str) -> gpd.GeoDataFrame:
     """
     Read shapefile vector data containing depth samples using Geopandas.
 
     Parameters
     ----------
-    vector_loc : str
+    vector_loc : Path | str
         Vector data location containing point depth samples.
 
     Returns
@@ -43,7 +44,7 @@ def read_shapefile(vector_loc: str) -> gpd.GeoDataFrame:
 
 def write_geotiff(
         raster: xr.DataArray,
-        raster_loc: str
+        raster_loc: Path | str
 ) -> None:
     """
     Write dataarray to Geotiff.
@@ -52,7 +53,7 @@ def write_geotiff(
     ----------
     raster : xr.DataArray
         Raster data in dataarray.
-    raster_loc : str
+    raster_loc : Path | str
         Raster save data location.
 
     Returns
@@ -65,7 +66,7 @@ def write_geotiff(
 
 def write_shapefile(
         table: pd.DataFrame,
-        vector_loc: str,
+        vector_loc: Path | str,
         x_col_name: str,
         y_col_name: str,
         crs: CRS | str | dict[str, Any],
@@ -78,7 +79,7 @@ def write_shapefile(
     ----------
     table : pd.DataFrame
         A dataframe containing XY coordinates.
-    vector_loc : str
+    vector_loc : Path | str
         Vector save data location.
     x_col_name : str
         X coordinates column name.
