31ec5f08702a3963aad9e3a36c3fe62034151c26
rifqiharrys <rifqi.m.h@gmail.com>
2025-09-27 16:18:11 +0700

    add gdal dir checker to remove gdal warning while running exe file

--- docs/exe/hook-gdal.py
+++ docs/exe/hook-gdal.py
@@ -2,4 +2,6 @@ import os
 import sys
 
 if getattr(sys, 'frozen', False):
-    os.environ['GDAL_DATA'] = os.path.join(sys._MEIPASS, 'gdal')
+    gdal_data = os.path.join(sys._MEIPASS, 'gdal') # type: ignore
+    if os.path.isdir(gdal_data):
+        os.environ['GDAL_DATA'] = gdal_data
