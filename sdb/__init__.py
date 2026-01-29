6a97e5b705a5540195bb8bf2a82ba6f9a45bc9e2
rifqiharrys <rifqiharrys@proton.me>
2026-01-29 17:50:23 +0700

    add new function and delete empty end line

--- sdb/__init__.py
+++ sdb/__init__.py
@@ -3,6 +3,6 @@ from .modeling import prediction
 from .postprocessing import (evaluate, out_depth_filter, reshape_prediction,
                              scatter_plotter)
 from .preprocessing import (clip_vector, features_label, in_depth_filter,
-                            reproject_vector, split_attribute, split_random,
-                            unravel)
+                            reproject_vector, split_attribute, split_data,
+                            split_random, unravel)
 from .utils import array_to_dataarray, median_filter, point_sampling
